166ccbf0dd92ed7629c0c6cd4ba71bb5d03cedc4
Raphael S. Dorta <raphael@sophus.lan>
2025-03-13 13:03:14 -0300

    Adiciona configuração de CORS para permitir requisições do GitHub Pages

--- vacinaApp/vacinaApp/src/main/java/com/senac/pi/vacinaApp/config/WebConfig.java
+++ vacinaApp/vacinaApp/src/main/java/com/senac/pi/vacinaApp/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.senac.pi.vacinaApp.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Permite todas as rotas da sua API
-                .allowedOrigins("https://raphadorta.github.io")  // Substitua com o seu domínio do GitHub Pages
+                .allowedOrigins("https://raphadorta.github.io")  // Substitua com o seu domínio GitHub Pages
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*");  // Permite todos os cabeçalhos
     }
